7e923757438f4cdcb8fac1c29f84e2de41f58091
Ricardo Cabral <rdmc69@gmail.com>
2017-02-08 00:11:05 -0100

    remove myscm.sh

--- myscm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-RDMC_CMTS_USER=ricardo; export RDMC_CMTS_USER
-RDMC_CMTS_PASS=zerty34sr71; export RDMC_CMTS_PASS
-RDMC_CMTS_ADDR=10.212.0.1; export RDMC_CMTS_ADDR
-#./scm 8494.8c0a.19c0
-./scm e448.c7bd.935a
-
